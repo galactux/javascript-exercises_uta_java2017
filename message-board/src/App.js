e73d7a0773fa93ffa885494a0afbd7e5aec72f52
galactux <galactuxx@gmail.com>
2017-07-01 12:23:32 +0300

    message-board: My coding

--- message-board/src/App.js
+++ message-board/src/App.js
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 import 'bootstrap/dist/css/bootstrap.css';
 import {
   Button,
+  ButtonToolbar,
   Col,
   ControlLabel,
   FormControl,
@@ -13,13 +14,26 @@ import {
 import Ilmoitus from './Ilmoitus';
 
 const socket = io();
+const dBase = 'ilmoitustaulu'; // Pouch database name
+const delay = 2000; // Extra db delay in milliseconds for testing purposes!
+
+// http://docs.couchdb.org/en/latest/api/
 
 class App extends Component {
   state = {
-    ilmoitukset: [],
-    teksti: ''
+    ilmoitukset: [], // All the documents from db
+    text: '', // Document message text
+    type: 'success', // Document type (success/warning/danger/info)
+    _id: '', // Document db id, has value only when an existing doc (stored in db) is being updated
+    _rev: '', // Document db revision, use only if _id is set
+    disable: false // "Disable" UI? True during an ongoing db operation
   };
-  componentWillMount() {
+
+  async componentWillMount() {
+    const data = await this.requestDB('/_all_docs?include_docs=true');
+    const ilmoitukset = data.rows.map(i => i.doc);
+    this.setState({ ilmoitukset });
+
     socket.on('change', doc => {
       if (doc._deleted) {
         // Dokumentti on poistettu.
@@ -42,33 +56,74 @@ class App extends Component {
         this.setState(this.state);
       }
     });
-    socket.emit('addListener', 'ilmoitustaulu');
+    socket.emit('addListener', dBase);
   }
+
   componentWillUnmount() {
-    socket.emit('removeListener', 'ilmoitustaulu');
+    socket.emit('removeListener', dBase);
   }
-  onSubmit = async event => {
+
+  onSubmit = event => {
     event.preventDefault();
-    // const teksti = this.state.teksti
-    const { teksti } = this.state;
-    // http://docs.couchdb.org/en/latest/api/database/common.html#post--db
-    await fetch('/ilmoitustaulu', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify({ teksti })
-    });
-    // Tyhjennetään tallennettu arvo tekstikentästä.
-    this.setState({ teksti: '' });
+    const { text, type, _id, _rev } = this.state;
+    if (!_id) {
+      // Create new doc
+      this.requestDB('', 'POST', { text, type, time: Date.now() });
+    } else {
+      // Update existing doc, keep original timestamp
+      this.requestDB(`/${_id}?rev=${_rev}`, 'PUT', {
+        text,
+        type,
+        time: this.state.ilmoitukset.find(
+          ({ _id, _rev }) => _id === this.state._id && _rev === this.state._rev
+        ).time
+      });
+    }
   };
+
+  async requestDB(url, method = 'GET', data = undefined) {
+    this.setState({ disable: true }); // "Lock" UI
+    // Wait for response headers
+    let resp = await fetch(
+      `/${dBase}${url}`,
+      Object.assign(
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: method
+        },
+        data ? { body: JSON.stringify(data) } : {}
+      )
+    );
+    // Don't care about status code, just wait for response JSON object
+    resp = await resp.json();
+    setTimeout(() => {
+      this.setState({ text: '', _id: '', disable: false }); // "Release" UI
+    }, delay);
+    return resp;
+  }
+
   render() {
     return (
       <Grid fluid>
         <h1>Ilmoitustaulu</h1>
         <Row>
           <Col xs={12} sm={7} md={8} lg={9}>
-            {this.state.ilmoitukset.map(Ilmoitus)}
+            {this.state.ilmoitukset.sort((a, b) => a.time - b.time).map(i => {
+              const { text, type, _id, _rev } = i;
+              const update = _id === this.state._id;
+              return Ilmoitus(
+                Object.assign({}, i, {
+                  disable: this.state.disable || (!!this.state._id && !update),
+                  update,
+                  updateFunc: () => this.setState({ text, type, _id, _rev }),
+                  cancelUpdFunc: () => this.setState({ text: '', _id: '' }),
+                  deleteFunc: () =>
+                    this.requestDB(`/${_id}?rev=${_rev}`, 'DELETE')
+                })
+              );
+            })}
           </Col>
           <Col xs={12} sm={5} md={4} lg={3}>
             <form onSubmit={this.onSubmit}>
@@ -77,13 +132,38 @@ class App extends Component {
                 <FormControl
                   type="text"
                   placeholder="Viesti"
-                  value={this.state.teksti}
+                  value={this.state.text}
+                  disabled={this.state.disable}
                   onChange={event =>
-                    this.setState({ teksti: event.target.value })}
+                    this.setState({ text: event.target.value })}
                 />
+                <ControlLabel>Tyyppi</ControlLabel>
+                <ButtonToolbar>
+                  {[
+                    ['success', 'OK'],
+                    ['warning', 'Varoitus'],
+                    ['danger', 'Vaara'],
+                    ['info', 'Info']
+                  ].map((t, k) =>
+                    <Button
+                      key={k}
+                      active={t[0] === this.state.type}
+                      bsSize={t[0] === this.state.type ? 'small' : 'xsmall'}
+                      bsStyle={t[0]}
+                      disabled={this.state.disable}
+                      onClick={() => this.setState({ type: t[0] })}
+                    >
+                      {t[1]}
+                    </Button>
+                  )}
+                </ButtonToolbar>
               </FormGroup>
-              <Button bsStyle="primary" type="submit" block>
-                Lähetä
+              <Button
+                className="btn-primary btn-block"
+                disabled={this.state.disable || !this.state.text}
+                type="submit"
+              >
+                {this.state._id ? 'Päivitä' : 'Lähetä'}
               </Button>
             </form>
           </Col>
--- message-board/src/Ilmoitus.js
+++ message-board/src/Ilmoitus.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import { Alert } from 'react-bootstrap';
+import { Alert, Button } from 'react-bootstrap';
 
-const Ilmoitus = ({ _id, teksti }) =>
-  <Alert key={_id}>
-    {teksti}
+const Ilmoitus = ({
+  _id,
+  text,
+  type,
+  disable,
+  update, // Existing doc is being updated?
+  updateFunc,
+  cancelUpdFunc,
+  deleteFunc
+}) =>
+  <Alert key={_id} bsStyle={type}>
+    {update
+      ? <div>
+          <mark>{text}</mark>
+          <Button
+            className="btn-primary btn-xs pull-right"
+            disabled={disable}
+            onClick={cancelUpdFunc}
+          >
+            Älä päivitä
+          </Button>
+        </div>
+      : <div>
+          {text}
+          <Button
+            className="btn-danger btn-xs pull-right"
+            disabled={disable}
+            onClick={deleteFunc}
+          >
+            Poista
+          </Button>
+          <Button
+            className="btn-primary btn-xs pull-right"
+            disabled={disable}
+            onClick={updateFunc}
+          >
+            Muuta
+          </Button>
+        </div>}
   </Alert>;
 
 export default Ilmoitus;
